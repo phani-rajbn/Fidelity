b5fce1e755e697b390cb10f09f8481570e086389
Phaniraj B.N <35284581+phani-rajbn@users.noreply.github.com>
2021-09-07 12:39:48 +0530

    Add files via upload

---
+++ Day02/MyConsole.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace SampleConApp.Day02
+{
+    class MyConsole
+    {
+        public static int GetNumber(string question)
+        {
+            int result;
+        TRYAGAIN:
+            Console.WriteLine(question);
+            if (!int.TryParse(Console.ReadLine(), out result))
+            {
+                Console.WriteLine("Input value should be a valid number");
+                goto TRYAGAIN;
+            }
+            return result;
+        }
+
+        public static double GetDouble(string question)
+        {
+            double result;
+        TRYAGAIN:
+            Console.WriteLine(question);
+            if (!double.TryParse(Console.ReadLine(), out result))
+            {
+                Console.WriteLine("Input value should be a valid number");
+                goto TRYAGAIN;
+            }
+            return result;
+        }
+
+        public static DateTime GetDate(string question)
+        {
+            DateTime result;
+        TRYAGAIN:
+            Console.WriteLine(question);
+            if (!DateTime.TryParse(Console.ReadLine(), out result))
+            {
+                Console.WriteLine("Given value is not a valid Date");
+                goto TRYAGAIN;
+            }
+            return result;
+        }
+    }
+}
---
+++ Day02/ObjectsDemo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleConApp.Day02
+{
+    class ObjectsDemo
+    {
+        static void Main(string[] args)
+        {
+            //Type Initializer in C# 4.0
+            Employee emp = new Employee { EmpID = 111, EmpAddress ="Bangalore", EmpName ="Phaniraj" };//creating object in C#....
+            
+            Console.WriteLine(emp.EmpID);//get
+            Console.WriteLine(emp.EmpName);//get
+            Console.WriteLine(emp.EmpAddress);//get
+        }
+    }
+}
+//Leaning assignment: SOLID Principles of OOP.
