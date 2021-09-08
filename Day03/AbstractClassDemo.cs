114c592040ef5dd8db85bc8d5bb71336ec91a86e
Phaniraj B.N <35284581+phani-rajbn@users.noreply.github.com>
2021-09-08 12:44:00 +0530

    Add files via upload

---
+++ Day03/AbstractClassDemo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+/*
+ * Abstract class is one that has one or more abstract methods in them.
+ * This class will contain certain methods that are not clear in implementation and will allow the derived classes to implement them which can provide the more presise way of implementing it.
+ * When U provide a modifier called abstract to a class, it is not-instantiatable.
+ * 
+ */
+namespace SampleConApp.Day03
+{
+    abstract class AbsClass
+    {
+        public void NormalMethod() => Console.WriteLine("Implemented here itself!!!!!");
+        public virtual void VirtualMethod() => Console.WriteLine("Implemented here but can be modified by derived");
+        public abstract void AbstractMethod();//Not clear on its implementation
+    }
+    //U must implement all the abstract methods of the class, else it should also be marked as abstract
+    class ImplAbsClass : AbsClass
+    {
+        public override void AbstractMethod()
+        {
+            Console.WriteLine("Implemented more presisely in the derived class");//Now clear and will be implemented. 
+        }
+    }
+    class AbstractClassDemo
+    {
+        static void Main(string[] args)
+        {
+            AbsClass cls = new ImplAbsClass();
+            cls.NormalMethod();
+            cls.VirtualMethod();
+            cls.AbstractMethod();
+        }
+    }
+}
---
+++ Day03/ObjectSlicing.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SampleConApp.Day03
+{
+    class BaseType
+    {
+        public void BaseFunc() => Console.WriteLine("Base Func");
+    }
+
+    class DerivedType : BaseType
+    {
+        public void DerivedFunc() => Console.WriteLine("Derived Func");
+    }
+    class ObjectSlicing
+    {
+        static void Main(string[] args)
+        {
+            BaseType type = new BaseType();
+            type.BaseFunc();
+
+            BaseType example = new DerivedType();
+            example.BaseFunc();//New methods of the derived are inaccessible to the base type object.
+            if(example is DerivedType)//U should typecast it to the derived type and with it U should call the method. 
+            {
+                var temp = example as DerivedType;
+                temp.DerivedFunc();
+            }
+
+        }
+    }
+}
