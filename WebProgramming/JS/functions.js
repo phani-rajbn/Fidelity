3aaf2d6de294319e0af1ad39d56480fa115f3bae
Phaniraj B.N <35284581+phani-rajbn@users.noreply.github.com>
2021-09-24 17:00:43 +0530

    Add files via upload

---
+++ WebProgramming/JS/functions.js
@@ -0,0 +1,7 @@
+let subFunc = function(arg1, arg2) {
+    return arg1 - arg2;//Lambda expression in JS...
+}//Anonymous methods in js...
+
+let mulFunc = (arg1, arg2) => arg1 * arg2;
+
+let divFunc = (arg1, arg2) => arg1 / arg2;
