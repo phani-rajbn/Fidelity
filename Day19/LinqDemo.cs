aaa2836b98a7c49302be68461d3eab64104c5e13
Phaniraj B.N <35284581+phani-rajbn@users.noreply.github.com>
2021-09-29 17:03:58 +0530

    Add files via upload

--- Day19/LinqDemo.cs
+++ Day19/LinqDemo.cs
@@ -2,8 +2,8 @@
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//Traditional LINQ does not have APIs to  insert, delete or update. All LINQ operations are done on IEnumerable objects which are forward only and read only. LINQ is used mainly to query the data, not to perform the Data manipulation. 
+//In .NET 3.5, LINQ was extended on XML and SQL. With these new extensions, we can perform all kinds of operations including data manipulations. 
 
 namespace SampleDBApp.Day19
 {
@@ -13,10 +13,18 @@ namespace SampleDBApp.Day19
         public string EmpName { get; set; }
         public string EmpCity { get; set; }
         public int EmpSalary { get; set; }
+        public int DeptID { get; set; }
     }
+    class Dept
+    {
+        public string DeptName { get; set; }
+        public int DeptId { get; set; }
+    }
+
+
     static class FileData
     {
-        const string filename = @"../../Day19/EmpData.csv";
+        const string filename = @"C:\Users\phani\source\repos\Fidelity Training\SampleDBApp\bin\Debug\EmpData.csv";
         public static List<Employee> GetAllEmployees()
         {
             if (!File.Exists(filename))
@@ -43,8 +51,64 @@ namespace SampleDBApp.Day19
         static List<Employee> theList = FileData.GetAllEmployees();
         static void Main(string[] args)
         {
-            foreach(var emp in theList)
-                Console.WriteLine(emp.EmpName);
+            //readOnlyNames();
+            //readNamesWithCity();
+            //whereClauseExample();
+            //orderByClause();
+            //groupByClause();
+        }
+
+        private static void groupByClause()
+        {
+            var groups = from emp in theList
+                         group emp.EmpName by emp.EmpCity into gr
+                         orderby gr.Key
+                         select gr;
+            //nested loop iterate thro the groups and in each group another iteration
+            foreach(var gr in groups)
+            {
+                Console.WriteLine("The City: " + gr.Key);
+                foreach(var name in gr)
+                    Console.WriteLine(name);
+                Console.WriteLine("------------------------------------");
+            }
+        }
+
+        private static void orderByClause()
+        {
+            var results = from emp in theList
+                          orderby emp.EmpCity descending
+                          select emp.EmpCity;
+            foreach (var item in results.Distinct())//Display only distinct Cities in the collection. 
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static void whereClauseExample()
+        {
+            var data = from emp in theList
+                       where emp.EmpCity == "Chennai" && emp.EmpName.StartsWith("C")
+                       select emp.EmpName;
+            foreach (var name in data) Console.WriteLine(name);
+        }
+
+        private static void readNamesWithCity()
+        {
+            var data = from emp in theList
+                        select new { Name = emp.EmpName, Address = emp.EmpCity };
+            foreach (var item in data)
+            {
+                Console.WriteLine($"{item.Name} from {item.Address}");
+            }
+        }
+
+        private static void readOnlyNames()
+        {
+            var names = from emp in theList
+                        select emp.EmpName;  //SELECT EMPNAME FROM THELIST...
+            //LINQ query will always result a IEnumerable object which when iterated, will read the data.
+            foreach (var name in names) Console.WriteLine(name);
         }
     }
 }
---
+++ Day19/XLINQDemo.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace SampleDBApp.Day19
+{
+    class XLINQDemo
+    {
+        static void Main()
+        {
+            //convertListToXml();
+            //displayNamesFromFile();
+            //displayNamesAndSalaryFromFile();
+            //insertEmployeeToXml();
+            //insertEmployeeToXmlById();
+            //deleteEmployeeInXmlById();
+        }
+
+        private static void deleteEmployeeInXmlById()
+        {
+            Console.WriteLine("Enter the ID of the Employee to delete");
+            string id = Console.ReadLine();
+            XDocument doc = XDocument.Load("Employees.xml");
+            var foundElement = (from element in doc.Descendants("Employee")
+                                where element.Element("EmpID").Value == id
+                                select element).SingleOrDefault();
+            foundElement.Remove();//Delete the Element. 
+            doc.Save("Employees.xml");
+        }
+
+        private static void insertEmployeeToXmlById()
+        {
+            Console.WriteLine("Enter the ID of the Employee where U want to add");
+            string id = Console.ReadLine();
+            XDocument doc = XDocument.Load("Employees.xml");
+            var selectedElement = (from element in doc.Descendants("Employee")
+                          where element.Element("EmpID").Value == id
+                          select element).FirstOrDefault();
+            if(selectedElement == null)
+            {
+                Console.WriteLine("No element found");
+            }
+            else
+            {
+                var newElement = new XElement("Employee",
+                        new XElement("EmpID", 502),
+                        new XElement("EmpName", "Rahul"),
+                        new XElement("EmpCity", "Bangalore"),
+                        new XElement("EmpSalary", 75000)
+                    );
+                selectedElement.AddAfterSelf(newElement);
+                doc.Save("Employees.xml");
+            }
+        }
+
+        private static void insertEmployeeToXml()
+        {
+            XDocument doc = XDocument.Load("Employees.xml");
+            //where to insert: At the last..
+            var last = doc.Descendants("Employee").Last();
+            //data to insert
+            var element = new XElement("Employee",
+                        new XElement("EmpID", 501),
+                        new XElement("EmpName", "Sachin"),
+                        new XElement("EmpCity", "Mumbai"),
+                        new XElement("EmpSalary", 65000)
+                        );
+            //insert the record
+            last.AddAfterSelf(element);//adds the element after the last
+            //save to the file.
+            doc.Save("Employees.xml");
+            //try to insert the record based in the id.
+        }
+
+        private static void displayNamesAndSalaryFromFile()
+        {
+            XDocument doc = XDocument.Load("Employees.xml");
+            var results = from element in doc.Descendants("Employee")
+                          select new
+                          {
+                              Name = element.Element("EmpName").Value,
+                              Salary = element.Element("EmpSalary").Value
+                          };
+            foreach (var emp in results) Console.WriteLine(emp);
+        }
+
+        private static void displayNamesFromFile()
+        {
+            XDocument doc = XDocument.Load("Employees.xml");
+            var elements = from element in doc.Descendants("Employee")
+                           select element.Element("EmpName");
+            foreach (var element in elements) Console.WriteLine(element.Value);
+        }
+
+        //Example to convert List<Employee> to Xml data using XLINQ. 
+        private static void convertListToXml()
+        {
+            var data = FileData.GetAllEmployees();//List of Employee objects.
+            var root = new XElement("ListOfEmployees", from emp in data
+                                                       select new XElement("Employee",
+                                                       new XElement("EmpID", emp.EmpID),
+                                                       new XElement("EmpName", emp.EmpName),
+                                                       new XElement("EmpCity", emp.EmpCity),
+                                                       new XElement("EmpSalary", emp.EmpSalary)
+                                                       ));
+            root.Save("Employees.xml");
+        }
+    }
+}
