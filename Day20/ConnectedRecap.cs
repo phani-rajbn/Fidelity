908387145e4b4d514d79a2c4592ecf90e33e2126
Phaniraj B.N <35284581+phani-rajbn@users.noreply.github.com>
2021-10-01 11:20:37 +0530

    Add files via upload

---
+++ Day20/ConnectedRecap.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+/*ADO.NET is a technology based on .NET for developing data access to both relational and non-relational databases. 
+ Connected model is a way of accessing the database using ADO.NET to allow interactions from the .NET App to the database with constant live connection. If the connection is closed, the interactions will not happen. 
+  Limits: U can access live data all the time. Too many connections can crash the app or will not allow database access. Suited for windows based apps and intranet based applications.
+A Connection class is required to make the connectivity to the database. 
+A Command class to pass the SQL Commands to the database. 
+A Reader class to read the data extracted from the SELECT commands made to the database from the Command object.
+3 ways to execute statement: 
+ExecuteReader: Used while passing SELECT Statements which return the data in the form of Reader object.  
+ExecuteScalar: Used for returning scalar values: only one row or one data.  
+ExecuteNonQuery. Used when U pass INSERT, DELETE, UPDATE statements. No return data is expected in these commands except the no of rows affected by the execution. 
+ */
+namespace SampleDBApp.Day20
+{
+    class ConnectedRecap
+    {
+        static string strCon = ConfigurationManager.ConnectionStrings["myCon"].ConnectionString;
+        const string query = "SELECT Movie.Title, Director.DirectorName FROM MOVIE INNER JOIN DIRECTOR on Movie.DirectorID = Director.DirectorID WHERE Director.DirectorName = @name";
+        static void Main(string[] args)
+        {
+            showmoviesByDirector("Atlee");
+        }
+
+        private static void showmoviesByDirector(string name)
+        {
+            SqlConnection con = new SqlConnection(strCon);
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@name", name);
+            try
+            {
+                con.Open();
+                var reader = cmd.ExecuteReader();
+                while(reader.Read())
+                    Console.WriteLine($"{reader[0]} -{reader[1]}");
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Dispose();
+            }
+        }
+    }
+}
---
+++ Day20/LINQToSQLDemo.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+//LINQ to SQL is a ORM framework for interacting with SQL server. Object relational mapping allows programmers to access the databases without a need of any SQL based language. they use object oriented classes to represent data and collections to store large amount of data. The ORM will have APIs to do all kinds of CRUD operations and query based functions. 
+//2 ORM Frameworks in .NET: LINQ to SQL and Entity Framework(MVC). LINQ To SQL is used only if U have SQL server in Ur application and Entity framework is used all kinds of databases.
+//DataContext class will be the name of the DBML file suffixed with DataContaxt. DataContext is the LINQ provided class that connects to the database and does all the operations. 
+namespace SampleDBApp.Day20
+{
+    class LINQToSQLDemo
+    {
+        static DataManagerDataContext context = new DataManagerDataContext();
+        static void Main(string[] args)
+        {
+            //firstDemo();
+            //showEmployeesWithDept();
+            //showEmployeesOfDept("Developement");
+            //orderbyFirstLetter();
+            //insertRecord();
+            //insertBulkRecords();
+            //deleteRecord();
+            //updateRecords();
+        }
+
+        private static void updateRecords()
+        {
+            var empData = context.Employees.Where((e) => e.DeptId == 2).ToList();
+            for (int i = 0; i < empData.Count; i++)
+            {
+                if (i % 3 == 0)
+                    empData[i].DeptId = 3;
+                else
+                    empData[i].DeptId = 5;
+            }
+            context.SubmitChanges();
+        }
+
+        private static void deleteRecord()
+        {
+            /*
+             * Take input for the Id to delete.
+             * query to find the record with matching id.
+             * call the deleting api of LINQ.
+             * save the changes
+             * take break after this
+             */
+            int id = 6;
+            //var rec = (from emp in context.Employees
+            //           where emp.EmpID == id
+            //           select emp).SingleOrDefault();
+            var rec = context.Employees.FirstOrDefault((e) => e.EmpID == id);
+            if (rec == null) Console.WriteLine("This rec does not exist");
+            else
+            {
+                context.Employees.DeleteOnSubmit(rec);
+                context.SubmitChanges();
+                Console.WriteLine("Employee deleted");
+            }
+            
+        }
+
+        /*
+* Windows 10 with 8GB RAM and Admin Rights.
+* Visual Studio Community 2019.
+* SQL Server Express Edition. 
+* VS Code
+*/
+        private static void insertBulkRecords()
+        {
+            XDocument doc = XDocument.Load("Employees.xml");
+            var records = (from element in doc.Descendants("Employee")
+                           select new Employee
+                           {
+                               DeptId = 2,
+                               EmpAddress = element.Element("EmpCity").Value,
+                               EmpSalary = int.Parse(element.Element("EmpSalary").Value),
+                               EmpName = element.Element("EmpName").Value
+                           }).Take(20);
+            context.Employees.InsertAllOnSubmit(records);
+            context.SubmitChanges();
+        }
+
+        private static void insertRecord()
+        {
+            //create the employee object to add...
+            var emp = new Employee { DeptId = 1, EmpAddress = "Mysore", EmpName = "Sudharshan", EmpSalary = 50000 };
+            context.Employees.InsertOnSubmit(emp);//add the employee object to the employees collection of the context.
+            context.SubmitChanges();//Commit the operation. 
+        }
+
+        private static void orderbyFirstLetter()
+        {
+            var groups = from emp in context.Employees
+                         group emp by emp.EmpName[0] into g
+                         orderby g.Key
+                         select g;
+            foreach(var gr in groups)
+            {
+                Console.WriteLine("Employee Names starting with letter: " + gr.Key);
+                foreach (var emp in gr)
+                {
+                    Console.WriteLine($"{emp.EmpName} from {emp.EmpAddress}");
+                }
+            }
+        }
+
+        private static void showEmployeesOfDept(string deptName)
+        {
+            var result = (from dept in context.Depts
+                         where dept.DeptName == deptName
+                         select dept.Employees).FirstOrDefault();
+            foreach(var emp in result)
+                Console.WriteLine(emp.EmpName);
+        }
+
+        private static void showEmployeesWithDept()
+        {
+            var result = from emp in context.Employees
+                         select new
+                         {
+                            Name = emp.EmpName,
+                            Dept = emp.Dept.DeptName
+                         };
+            foreach (var res in result) Console.WriteLine(res);
+        }
+
+        private static void firstDemo()
+        {
+            var data = from cst in context.Customers
+                       where cst.City == "Bangalore"
+                       orderby cst.CustomerName descending
+                       select cst.CustomerName;
+            foreach (var name in data) Console.WriteLine(data);
+        }
+    }
+}
